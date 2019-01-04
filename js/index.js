76c3022c9f674dea95d4b84569f70747e3349006
ryan m <contact@ryanmeek.com>
2019-01-04 23:50:45 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
 divs = document.querySelectorAll('div');
- 
+
 function capture(e) {
   console.log(this.firstChild.nodeValue.trim() + ' captured');
 }
- 
+
 for (let i = 0; i < divs.length; i++) {
   divs[i].addEventListener('click', capture, true);
   if (divs[i].style.backgroundColor != '#fff')
