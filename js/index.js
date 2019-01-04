811e294eccd2ec1b119dc99fbe9b1a0bbde9b37e
ryan m <contact@ryanmeek.com>
2019-01-04 23:49:15 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,2 +1,14 @@
-// using this file is optional
-// you can also load the code snippets in using your browser's console
+divs = document.querySelectorAll('div');
+ 
+function capture(e) {
+  console.log(this.firstChild.nodeValue.trim() + ' captured');
+}
+ 
+for (let i = 0; i < divs.length; i++) {
+  divs[i].addEventListener('click', capture, true);
+  if (divs[i].style.backgroundColor != '#fff')
+    divs[i].style.backgroundColor = '#fff';
+  } else {
+    divs[i].style.backgroundColor = '#000'
+  }
+}
