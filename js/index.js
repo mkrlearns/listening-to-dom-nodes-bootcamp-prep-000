8a60d632594344c1386dcce22719f25c979c2736
ryan m <contact@ryanmeek.com>
2019-01-05 00:00:52 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -13,9 +13,9 @@ function capture(e) {
 
 for (let i = 0; i < divs.length; i++) {
   divs[i].addEventListener('click', capture, true);
-  if (divs[i].style.backgroundColor != '#fff') {
-    divs[i].style.backgroundColor = '#fff';
-  } else {
-    divs[i].style.backgroundColor = '#000'
-  }
+  // if (divs[i].style.backgroundColor != '#fff') {
+  //   divs[i].style.backgroundColor = '#fff';
+  // } else {
+  //   divs[i].style.backgroundColor = '#000'
+  // }
 }
