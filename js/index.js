57bbf9f964d1947eaa1ee6928e7b362a751c21f1
ryan m <contact@ryanmeek.com>
2019-01-05 00:03:39 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -3,10 +3,10 @@ divs = document.querySelectorAll('div');
 function capture(e) {
   console.log(this.firstChild.nodeValue.trim() + ' captured');
   for (let i = 0; i < divs.length; i++) {
-    if (divs[i].style.backgroundColor != '#fff') {
-      divs[i].style.backgroundColor = '#fff';
+    if (divs[i].style.backgroundColor != '#000') {
+      divs[i].style.backgroundColor = '#000';
     } else {
-      divs[i].style.backgroundColor = '#000'
+      divs[i].style.backgroundColor = '#fff'
     }
   }
 }
