679752bed5a23f9855571399dda4e392eeecd4b8
ryan m <contact@ryanmeek.com>
2019-01-05 00:01:25 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -2,20 +2,13 @@ divs = document.querySelectorAll('div');
 
 function capture(e) {
   console.log(this.firstChild.nodeValue.trim() + ' captured');
-  // for (let i = 0; i < divs.length; i++) {
-  //   if (divs[i].style.backgroundColor != '#fff') {
-  //     divs[i].style.backgroundColor = '#fff';
-  //   } else {
-  //     divs[i].style.backgroundColor = '#000'
-  //   }
-  // }
 }
 
 for (let i = 0; i < divs.length; i++) {
   divs[i].addEventListener('click', capture, true);
-  // if (divs[i].style.backgroundColor != '#fff') {
-  //   divs[i].style.backgroundColor = '#fff';
-  // } else {
-  //   divs[i].style.backgroundColor = '#000'
-  // }
+  if (divs[i].style.backgroundColor != '#fff') {
+    divs[i].style.backgroundColor = '#fff';
+  } else {
+    divs[i].style.backgroundColor = '#000'
+  }
 }
