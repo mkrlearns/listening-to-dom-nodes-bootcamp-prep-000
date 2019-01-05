9e98a3aef79791b43e7e756690db5475187001ac
ryan m <contact@ryanmeek.com>
2019-01-05 00:12:22 +0000

    Done.

--- js/index.js
+++ js/index.js
@@ -1,2 +1,24 @@
-// using this file is optional
-// you can also load the code snippets in using your browser's console
+divs = document.querySelectorAll('div');
+let black;
+console.log('test');
+
+function capture(e) {
+  console.log(this.firstChild.nodeValue.trim() + ' captured');
+  if (black) black = false
+  else black = true
+  for (let i = 0; i < divs.length; i++) {
+    if (black) {
+      divs[i].style.backgroundColor = '#fff';
+    } else {
+      divs[i].style.backgroundColor = '#000';
+    }
+  }
+}
+
+for (let i = 0; i < divs.length; i++) {
+  divs[i].addEventListener('click', capture, true);
+}
+
+function bw() {
+
+}
